598871eb1c8cdd8c85fe2a09aca8203ce02c21e3
conconrob <connortprobertson@gmail.com>
2019-03-19 17:28:14 +0000

    made mw only capitalize the first letter

--- UpperCaseNameMW.js
+++ UpperCaseNameMW.js
@@ -1,6 +1,6 @@
 function upperCaseName(req, res, next){
-  if((req.method==="POST"||req.method==="PUT") && req.body){
-    req.body.name = req.body.name.toUpperCase();
+  if((req.method==="POST"||req.method==="PUT") && req.body.name){
+    req.body.name = req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1);
   }
   next()
 }
